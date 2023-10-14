7305f7a6990052597cbf7567a0a96839037c98ee
Fábio Teixeira <fabio.teixeira@ipremi.com.br>
2023-10-14 20:01:00 -0300

    Conhecendo as diretivas

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,15 +1,24 @@
 <template>
-    <p>estou trabalhando no momento.</p>
+    <p v-if="estado_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidade!</p>
     <p>Utilizo as seguintes tecnlogias:</p>
     <ul>
         <li>PHP / Laravel</li>
         <li>Mysql</li>
         <li>Iniciando com vueJS</li>
     </ul>
+    <p v-show="exibir">Mande uma mensagem para: {{ email}}</p>
 </template>
 
 <script>
 export default {
-    name: "Info"
+    name: "Info",
+    data() {
+        return{
+            estado_trabalhando: true,
+            exibir: true,
+            email: 'fabio@email.com'
+        }
+    }
 }
 </script>
